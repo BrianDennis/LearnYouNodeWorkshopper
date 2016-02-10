4434ec73ffc5328f0a68d6b34cae4361a00505ba
Brian Dennis <briandennis321@gmail.com>
2016-02-10 15:57:54 -0600

    deleted old folder

--- Exercise Files/firstIo.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var filePath = process.argv[2];
-
-var fileBuffer = fs.readFileSync(filePath);
-var stringBuffer = fileBuffer.toString();
-var newLineArray = stringBuffer.split('\n');
-console.log(newLineArray.length-1);
